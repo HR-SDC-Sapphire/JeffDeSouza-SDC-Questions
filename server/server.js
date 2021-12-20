878fefbcd69299c9c9e10b9aa3d36b9527958464
Jeff DeSouza <jeffdesouzadev@gmail.com>
2021-12-20 16:45:31 -0600

    index sanity check attempt 2

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 app.use(bodyParser.json())
-app.use(express.static('public'));
+app.use(express.static(__dirname + 'public'));
 const mongoose = require('mongoose');
 const questionsSchema = require('../database/models/questions.js')
 const Question = mongoose.model('questions', questionsSchema);
